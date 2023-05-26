0d67f0a6b3a2e8c8c2f8a0e525d23a8aecb3e57c
daniel <over@Shuhkz-WS.localdomain>
2023-05-26 13:36:25 +0200

    writing tests for urls and reverse_urls

--- project/pages/tests.py
+++ project/pages/tests.py
@@ -1,3 +1,25 @@
 from django.test import TestCase
 
 # Create your tests here.
+
+from django.test import SimpleTestCase
+from django.urls import reverse
+
+import sys
+sys.tracebacklimit = 0
+
+class HomeTests( SimpleTestCase):
+	def test_url_exists_at_correct_location( s):
+		response = s.client.get('/')
+		s.assertEqual( response.status_code, 200)
+	def test_url_available_by_name( s):
+		response = s.client.get( reverse( 'home'))
+		s.assertEqual( response.status_code, 200)
+
+class AboutTests( SimpleTestCase):
+	def test_url_exists_at_correct_location( s):
+		response = s.client.get('/about/')
+		s.assertEqual( response.status_code, 200)
+	def test_url_available_by_name( s):
+		response = s.client.get( reverse( 'about'))
+		s.assertEqual( response.status_code, 200)
