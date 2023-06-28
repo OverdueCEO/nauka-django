a47da3a0d6f85e17b1648a8b397f6446685b7eef
daniel <over@Shuhkz-WS.localdomain>
2023-06-28 12:33:17 +0200

    add template tests

--- project/pages/tests.py
+++ project/pages/tests.py
@@ -8,6 +8,7 @@ from django.urls import reverse
 import sys
 sys.tracebacklimit = 0
 
+
 class HomeTests( SimpleTestCase):
 	def test_url_exists_at_correct_location( s):
 		response = s.client.get('/')
@@ -15,6 +16,12 @@ class HomeTests( SimpleTestCase):
 	def test_url_available_by_name( s):
 		response = s.client.get( reverse( 'home'))
 		s.assertEqual( response.status_code, 200)
+	def test_template_name_is_correct( s):
+		response = s.client.get( reverse('home'))
+		s.assertTemplateUsed( response, 'home.html')
+	def test_template_content( s):
+		response = s.client.get( reverse( 'home'))
+		s.assertContains( response, '<h1>Homepage</h1>')
 
 class AboutTests( SimpleTestCase):
 	def test_url_exists_at_correct_location( s):
@@ -23,3 +30,9 @@ class AboutTests( SimpleTestCase):
 	def test_url_available_by_name( s):
 		response = s.client.get( reverse( 'about'))
 		s.assertEqual( response.status_code, 200)
+	def test_template_name_correct( s):
+		response = s.client.get( reverse( 'about'))
+		s.assertTemplateUsed( response, 'about.html')
+	def test_template_content( s):
+		response = s.client.get( reverse( 'about'))
+		s.assertContains( response, '<h1>About page</h1>')
